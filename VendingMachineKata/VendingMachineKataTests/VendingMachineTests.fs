905af9ec364ff28c2f645a796080a1324a37a25e
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-08-31 20:27:22 +0200

    Refactor remove test duplication

--- VendingMachineKata/VendingMachineKataTests/VendingMachineTests.fs
+++ VendingMachineKata/VendingMachineKataTests/VendingMachineTests.fs
@@ -6,34 +6,34 @@ open VendingMachineKata
 type VendingMachineTests() =
     
     let mutable vm = new VendingMachine()
+    let insert coin =
+        vm.Insert coin |> ignore
+
     [<TestInitialize>]
     member this.``New vending machine for each test``()=
         vm <- new VendingMachine()
 
     [<TestMethod>]
-    member this.``Valid Coins are accepted``()=
+    member this.``Only Valid Coins are accepted``()=
+        Assert.IsFalse(vm.Insert Penny)
         Assert.IsTrue(vm.Insert Dime)
         Assert.IsTrue(vm.Insert Nickel)
         Assert.IsTrue(vm.Insert Quarter)
 
     [<TestMethod>]
-    member this.``Pennies are rejected``()=
-        Assert.IsFalse(vm.Insert Penny)
-
-    [<TestMethod>]
     member this.``Accepted coins updates display``()=
         Assert.AreEqual("INSERT COIN", vm.Display)
-        vm.Insert Dime |> ignore
+        insert Dime
         Assert.AreEqual("5", vm.Display)
-        vm.Insert Nickel |> ignore
+        insert Nickel
         Assert.AreEqual("15", vm.Display)
-        vm.Insert Quarter |> ignore
+        insert Quarter
         Assert.AreEqual("40", vm.Display)
 
     [<TestMethod>]
     member this.``Rejected coins are returned``()=
-        vm.Insert(Penny) |> ignore
-        vm.Insert(Penny) |> ignore
+        insert Penny
+        insert Penny
         let returnedCoins = vm.TakeCoinReturn
         Assert.AreEqual([Penny;Penny;], returnedCoins)
 
