2cd9c7a7cb6bd856ed96ad3db5fbaa82c017c222
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-08-31 20:09:43 +0200

    Initial machine display is INSERT COIN

--- VendingMachineKata/VendingMachineKataTests/VendingMachineTests.fs
+++ VendingMachineKata/VendingMachineKataTests/VendingMachineTests.fs
@@ -22,9 +22,11 @@ type VendingMachineTests() =
 
     [<TestMethod>]
     member this.``Accepted coins updates display``()=
+        Assert.AreEqual("INSERT COIN", vm.Display)
         vm.Insert Dime |> ignore
         Assert.AreEqual("5", vm.Display)
         vm.Insert Nickel |> ignore
         Assert.AreEqual("15", vm.Display)
         vm.Insert Quarter |> ignore
         Assert.AreEqual("40", vm.Display)
+
