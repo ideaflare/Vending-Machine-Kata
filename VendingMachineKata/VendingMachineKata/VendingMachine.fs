a3e91ed2c3f0854a5fa9a7b6d0192d21af28aa92
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-08-31 20:01:59 +0200

    Accepted coins adds to avaliable credit

--- VendingMachineKata/VendingMachineKata/VendingMachine.fs
+++ VendingMachineKata/VendingMachineKata/VendingMachine.fs
@@ -4,8 +4,23 @@ type Coin = Penny | Dime | Nickel | Quarter
 
 type VendingMachine() = 
     
+    let mutable amount = 0
+
+    let coinValue coin = 
+        match coin with
+        | Penny -> 1
+        | Dime -> 5
+        | Nickel -> 10
+        | Quarter -> 25
+
     member this.Insert coin =
         match coin with
         | Penny -> false
-        | _ -> true
+        | _ -> 
+            amount <- amount + (coinValue coin)
+            true
         
+    member this.Display
+        with get() =
+            if amount > 0 then (string amount)
+            else "INSERT COIN"
